44107bf8e8a9016e94fb265c7e111ec8c275943b
tmack22 <tmack@seattleu.edu>
2022-08-17 22:55:34 -0700

    Adding summary df

--- Code/Q2_Code.R
+++ Code/Q2_Code.R
@@ -18,12 +18,27 @@ data <- read_dta('./Industry_data.dta.gz')
   
 df <- inner_join(data,industry_names,by= "ind") %>%
       mutate(cpsid = format(cpsid,scientific= FALSE)) %>%
-      mutate(cpsidp = format(cpsidp,scientific= FALSE))
+      mutate(cpsidp = format(cpsidp,scientific= FALSE)) %>%
+      mutate(year_month = paste(year,"-",month,sep= '')) %>%
+      mutate(unique_id = paste(serial,pernum,sep= ''))
 
-filtered_df <- df %>% group_by(year, month, indname,cpsidp) %>%
+filtered_df <- df %>% group_by(year_month, indname,cpsidp) %>%
+              # drop_na(asecwt) %>%
+               mutate(n = n()) %>%
+               filter(n == 1) #to filter out all duplicate entries
+filtered_df <- filtered_df[-c(ncol(filtered_df))] #removing the n column
+
+filtered_out_df <- df %>% group_by(year_month, indname,unique_id) %>%
+                   mutate(n = n()) %>%
+                   filter(n != 1)   
+
+summary <- filtered_df %>%
+           ungroup(year,cpsidp) %>% 
+           select(c("year_month","indname")) %>%
+           group_by(year_month,indname) %>%
            mutate(n = n()) %>%
-           filter(n != 1) #to filter out all duplicate entries
-           
+           distinct()
+
 
 
 
