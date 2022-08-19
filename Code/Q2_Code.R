44de197ee9e29acfe5d0e928abf0f1bc58605058
tmack22 <tmack@seattleu.edu>
2022-08-18 22:40:55 -0700

    Updating scripts

--- Code/Q2_Code.R
+++ Code/Q2_Code.R
@@ -14,28 +14,28 @@ industry_names <- read.csv('./indnames.csv') %>%
     indname == 'Retail Trade' ~ 1,
     indname != 'Retail Trade' ~ 0))
 
-data <- read_dta('./Industry_data.dta.gz')
-        
-  
+data <- read_dta('./cps_00004.dta.gz')
+
+
 df <- inner_join(data,industry_names,by= "ind") %>%
-      mutate(cpsid = format(cpsid,scientific= FALSE)) %>%
-      mutate(cpsidp = format(cpsidp,scientific= FALSE)) %>%
-      mutate(year_month = paste(year,"-", month,sep= '')) %>%
-      mutate(covid_active = case_when(year_month >= '2020-3' ~ 1,
-                                      year_month < '2020-3' ~ 0))
-      
+  mutate(cpsid = format(cpsid,scientific= FALSE)) %>%
+  mutate(cpsidp = format(cpsidp,scientific= FALSE)) %>%
+  mutate(year_month = paste(year,"-", month,sep= '')) %>%
+  mutate(covid_active = case_when(year_month >= '2020-3' ~ 1,
+                                  year_month < '2020-3' ~ 0))
+
 
 filtered_df <- df %>% filter(cpsid > 0) %>%
-                      group_by(year_month,indname) %>%
-                      mutate(n = n())
+  group_by(year_month,indname) %>%
+  mutate(n = n())
 
 filtered_out_df <- df %>% group_by(year_month, indname,cpsidp) %>%
-                   mutate(n = n()) %>%
-                   filter(n != 1)   
+  mutate(n = n()) %>%
+  filter(n != 1)   
 
 summary <- filtered_df %>%
-           select(c("year_month","indname","covid_active","n")) %>%
-           distinct()
+  select(c("year_month","indname","covid_active","n")) %>%
+  distinct()
 
 reg <- feols(n~indname * covid_active ,data=summary)
 etable(reg)
