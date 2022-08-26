f82a1be493dab3f87db156acfed719a162fc3eaa
tmack22 <tmack@seattleu.edu>
2022-08-26 16:09:39 -0700

    Uploading ggplots

--- Code/Q2_Code.R
+++ Code/Q2_Code.R
@@ -6,6 +6,7 @@ library(lubridate)
 library(estimatr)
 library(fixest)
 library(dplyr)
+library(ggplot2)
 
 
 setwd('./Data/')
@@ -20,52 +21,85 @@ data <- read_dta('./cps_00004.dta.gz')
 df <- inner_join(data,industry_names,by= "ind") %>%
   mutate(cpsid = format(cpsid,scientific= FALSE)) %>%
   mutate(cpsidp = format(cpsidp,scientific= FALSE)) %>%
-  mutate(year_month = paste(year,"-", month,sep= '')) %>%
-  mutate(covid_active = case_when(year_month >= '2020-3' ~ 1,
-                                  year_month < '2020-3' ~ 0))
-
+  mutate(date = as_date(paste(year, month, '01', sep = '-'))) %>%
+  mutate(covid_active = case_when(date >= '2020-03-01' ~ 1,
+                                  date < '2020-03-01' ~ 0))
 
 filtered_df <- df %>% filter(cpsidp > 0) %>%
   filter(empstat <20 & empstat >= 10) %>% #20 and above indicates they are unemployed/no looking
-  group_by(year_month,indname) %>%
+  group_by(date,indname) %>%
   mutate(n = n())
 
-industry_df <- filtered_df %>%
+industry_cat_df <- filtered_df %>%
                ungroup() %>%
-               select(c('industry','year_month','n')) %>%
+               select(c('industry','year','date','covid_active','n')) %>%
                distinct() %>%
-               group_by(industry,year_month) %>%
+               group_by(industry,date) %>%
                mutate(count = sum(n))%>%
-               select(-c(3))%>%
+               select(-c(5))%>%
                distinct() %>%
                ungroup()%>%
                group_by(industry)%>%
-               mutate(percent_change = ((count-lag(count))/lag(count)*100))%>%
-               mutate(percent_change = case_when(percent_change = is.na(percent_change) ~ 0,
-                                                 percent_change == is.na(percent_change) ~ 0,
-                                                 percent_change != is.na(percent_change) ~ percent_change))
-                                                  
+               mutate(percent_change = ((count-lag(count))/lag(count)))%>%
+               mutate(percent_change = case_when(
+                 percent_change = is.na(percent_change) ~ 0,
+                 percent_change == is.na(percent_change) ~ 0,
+                 percent_change != is.na(percent_change) ~ percent_change))
+
+industry_df <- filtered_df %>%
+  ungroup() %>%
+  select(c('indname','year','date','covid_active','n')) %>%
+  distinct() %>%
+  group_by(indname,date) %>%
+  mutate(count = sum(n))%>%
+  select(-c(5))%>%
+  distinct() %>%
+  ungroup()%>%
+  group_by(indname)%>%
+  mutate(percent_change = ((count-lag(count))/lag(count)))%>%
+  mutate(percent_change = case_when(
+    percent_change = is.na(percent_change) ~ 0,
+    percent_change == is.na(percent_change) ~ 0,
+    percent_change != is.na(percent_change) ~ percent_change))
+
+
+industry_cat_df %>%
+  ggplot(aes(x=date,y=percent_change,group=industry,color=industry)) +
+  scale_y_continuous(labels=scales::percent_format(accuracy=.1)) +
+  geom_point() + geom_line() + theme(aspect.ratio=6/7) +
+  labs(x="Date",y="Employment Percentage Change",color="Industry Category",
+       title= "Employment Percentage Change by Industry Category")
+
+industry_df %>%
+  ggplot(aes(x=date,y=percent_change,group=indname,color=indname)) +
+  scale_y_continuous(labels=scales::percent_format(accuracy=.1)) +
+  geom_point() + geom_line() + theme(aspect.ratio=1) +
+  labs(x="Date",y="Employment Percentage Change",color="Industry Name",
+       title= "Employment Percentage Change by Industry")
 
 
 summary <- filtered_df %>%
-  select(c("year_month","indname","covid_active","Industry","n")) %>%
-  distinct()
+  ungroup()%>%
+  group_by(date,industry)%>%
+  mutate(avg_employment = mean(n))%>%
+  select(c('industry','year','date','covid_active','avg_employment'))%>%
+  distinct()%>%
+  ungroup()%>%
+  group_by(industry)%>%
+  mutate(employment_change = ((avg_employment-lag(avg_employment))))%>%
+  drop_na()
 
-reg1 <- feols(n~indname + covid_active ,data= summary)
-reg2 <- feols(log(n)~indname + covid_active,data= summary)
-etable(reg1,reg2)
+#Production level regressions
+reg1 <- feols(n~indname,se= 'hetero',data= filtered_df)
+reg1.1 <- feols(n~indname*covid_active,se='hetero',data= filtered_df)
+etable(reg1,reg1.1)
 
-reg3 <- feols(n~Retail + covid_active,data= summary)
-reg4 <- feols(log(n)~Retail + covid_active,data= summary)
-reg5 <- feols(n~Retail,data= summary)
-reg6 <- feols(log(n)~Retail,data= summary)
-etable(reg3,reg4,reg5,reg6)
-etable(reg5)
+reg2 <- feols(n~industry,se='hetero',data= filtered_df)
+reg2.1 <- feols(n~industry*covid_active,se='hetero',data= filtered_df)
+etable(reg2,reg2.1)
 
-reg7 <- feols(n~ Retail*covid_active,data=summary)
-etable(reg7)
-wald(reg1)
-wald(reg2)
-wald(reg3)
-wald(reg4)
+reg3 <- feols(employment_change~industry,se= 'hetero',data= summary)
+reg3.1 <- feols(employment_change~industry*covid_active,se= 'hetero',data= summary)
+etable(reg3,reg3.1)
+#
 
